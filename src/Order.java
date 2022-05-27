dc87a6c55475be41c1f36c0c917db73d6d7ad09b
xnacly <matteogropp@protonmail.com>
2022-05-27 22:42:17 +0200

    refactor(Order): remove 'TypeOfTransaction' enum - remove 'toT' Order attribute

--- src/Order.java
+++ src/Order.java
@@ -1,26 +1,16 @@
 import product.Product;
 
-enum TypeOfTransaction {
-    RECYCLE,
-    REARRANGE,
-    INSERT,
-    REMOVE,
-    SKIP
-}
-
 public class Order {
     boolean insertOrder;
     Product product;
     int price;
     int id;
-    TypeOfTransaction toT;
 
     public Order(int id, boolean insertOrder, Product product, int price){
        this.id = id;
        this.insertOrder = insertOrder;
        this.product = product;
        this.price = price;
-       this.toT = insertOrder ? TypeOfTransaction.INSERT : TypeOfTransaction.REMOVE;
     }
 
     @Override
