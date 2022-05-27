d37015f3099ef278aef8454cd21eb5e8dfc6c933
xnacly <matteogropp@protonmail.com>
2022-05-27 22:39:24 +0200

    refactor(Balance): add 'getTransactions' - change transactios from ArrayList type 'Order' to 'String - change 'updateBalance' to add or remove the given amount based on the 'remove' parameter

--- src/Balance.java
+++ src/Balance.java
@@ -2,17 +2,23 @@ import java.util.ArrayList;
 
 public class Balance {
     private int balance;
-    private ArrayList<Order> transactions;
+    private ArrayList<String> transactions = new ArrayList<>();
 
     public Balance(){
         this.balance = 0;
     }
 
-    public void updateBalance(Order o, TypeOfTransaction typeOfTransaction){
-        transactions.add(o);
+    public void updateBalance(int amount, String log, boolean remove){
+        transactions.add(log);
+        if(remove) this.balance -= amount;
+        else this.balance += amount;
     }
 
     public int getBalance(){
         return this.balance;
+    }
+
+    public ArrayList<String> getTransactions(){
+        return this.transactions;
     }
 }
