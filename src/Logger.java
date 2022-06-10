7e90dc4816781b1a39496e77170d0cfe6ff876ba
xnacly <matteogropp@protonmail.com>
2022-06-10 16:32:46 +0200

    feat(Logger): add silent flag

--- src/Logger.java
+++ src/Logger.java
@@ -4,16 +4,16 @@ import java.util.Date;
 
 public class Logger {
     static boolean debug;
-    public Logger(boolean debug){
-        Logger.debug = debug;
-    }
+    static boolean silent;
+
     static private String getDate(){
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Date date = new Date();
         return dateFormat.format(date);
     }
+
     static void inf(String text){
-        System.out.printf("[INF][%s]: %s\n", Logger.getDate(), text);
+        if(!silent) System.out.printf("[INF][%s]: %s\n", Logger.getDate(), text);
     }
 
     static void err(String text){
@@ -21,9 +21,9 @@ public class Logger {
     }
 
     static void suc(String text){
-        System.out.printf("[SUC][%s]: %s\n", Logger.getDate(), text);
+        if(!silent) System.out.printf("[SUC][%s]: %s\n", Logger.getDate(), text);
     }
     static void debug(String text, int l, String file){
-        if(Logger.debug) System.out.printf("[DEBUG][%s][f:%s][l:%d]: %s\n", Logger.getDate(), file, l, text);
+        if(!silent) if(Logger.debug) System.out.printf("[DEBUG][%s][f:%s][l:%d]: %s\n", Logger.getDate(), file, l, text);
     }
 }
