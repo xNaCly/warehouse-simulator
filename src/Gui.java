3ab8e20384f24f0b349a42c2caddc14675c87a78
xnacly <matteogropp@protonmail.com>
2022-06-14 23:49:58 +0200

    feat: further improved the gui

--- src/Gui.java
+++ src/Gui.java
@@ -9,7 +9,12 @@ public class Gui {
     private Balance b;
     private Storage l;
     private JLabel balanceLabel;
+    private JLabel currentOrder;
     private JFrame transactionsWindow;
+    private int currentOpenOrders;
+    private ArrayList<Order> o;
+    private int currentOrderIndex;
+    private boolean popOutActive;
 
     public static void setUIFont (javax.swing.plaf.FontUIResource f){
         java.util.Enumeration keys = UIManager.getDefaults().keys();
@@ -22,80 +27,151 @@ public class Gui {
     }
 
     public Gui() {
+        JFrame.setDefaultLookAndFeelDecorated(true);
         this.r = new JFrame("Warehouse simulator");
         this.b = new Balance();
         this.l = new Storage(b);
 
         Fs fs = new Fs(Start.path);
-        ArrayList<Order> o = fs.parseOrders();
-        this.l.update(o.get(0), 0, 0, 0);
-        this.l.recycle(0,0,0);
+        this.o = fs.parseOrders();
         this.start();
         setUIFont(new FontUIResource(new Font("Serif",Font.PLAIN,18)));
     }
 
     private void start(){
-        this.r.setSize(400,500);
-
-        // stop running after gui is closed, if this isn't specified the process keeps running in the terminal
         this.r.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.r.setSize(600,600);
+        this.r.setLayout(new BorderLayout());
 
-        this.displayTransactionsButton();
-        this.displayBalance();
+        this.renderButtons();
+        this.hud();
 
-        this.r.setVisible(true);
         this.r.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent windowEvent){
                 System.exit(0);
             }
         });
+        this.r.setVisible(true);
     }
 
     private void popOutTransactions(){
+        this.popOutActive = true;
         this.transactionsWindow = new JFrame("Warehouse simulator - Transactions");
+        this.transactionsWindow.setSize(1024, 500);
+        this.transactionsWindow.setVisible(true);
+
         String[] headerRow = {"Transaction ID", "Transaction"};
         String[][] data = new String[this.b.getTransactions().size()][2];
         ArrayList<String> transactions = this.b.getTransactions();
 
         for(int i = 0; i < transactions.size(); i++){
             String s = transactions.get(i);
-            Logger.debug(s);
             data[i][0] = ""+i;
             data[i][1] = s;
         }
 
-        JTable table = new JTable(data, headerRow);
-        JScrollPane sp = new JScrollPane(table);
 
-        this.transactionsWindow.add(sp);
-        this.transactionsWindow.setSize(1024, 500);
-        this.transactionsWindow.setVisible(true);
+        JTable jt = new JTable(data, headerRow);
+        JScrollPane jp = new JScrollPane(jt);
+        this.transactionsWindow.add(jp);
+
         // remove all Lables in transaction window after closing it
         this.transactionsWindow.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent windowEvent){
                 transactionsWindow.removeAll();
+                transactionsWindow.setVisible(false);
+                popOutActive = false;
+                transactionsWindow.dispose();
             }
         });
     }
 
-    private void displayBalance(){
-        this.balanceLabel = new JLabel("Balance: " + this.b.getBalance() + "€");
-        this.r.add(balanceLabel);
+    private void hud(){
+        JPanel container = new JPanel();
+        container.setLayout(new BorderLayout());
+        this.balanceLabel = new JLabel();
+        this.currentOrder = new JLabel();
+        this.rerenderHud();
+        container.add(this.currentOrder, BorderLayout.WEST);
+        container.add(this.balanceLabel, BorderLayout.EAST);
+        this.r.add(container, BorderLayout.NORTH);
     }
 
-    private void displayTransactionsButton(){
-        JButton b = new JButton("View Transactions");
-        b.setActionCommand("View Transactions");
-        b.addActionListener(new ButtonClickListener());
-        b.setSize(250, 50);
-        this.r.add(b);
+    private void rerenderHud(){
+        this.currentOrder.setText(this.o.get(this.currentOrderIndex).toString().replace(":", " "));
+        this.balanceLabel.setText("Balance: " + this.b.getBalance() + "€");
+    }
+
+    private void renderMenu(){
+        JPanel container = new JPanel();
+        JButton fulFillOrder = new JButton("Auftrag erfüllen");
+        fulFillOrder.setActionCommand("fulfill");
+        fulFillOrder.setSize(250, 50);
+
+        container.add(fulFillOrder);
+        this.r.add(container);
+    }
+
+    private void renderButtons(){
+        JPanel container = new JPanel();
+        container.setLayout(new GridLayout());
+        JButton transactionButton = new JButton("Transaktionsliste");
+        JButton nextOrder = new JButton("Nächster Auftrag");
+        JButton skipOrder = new JButton("Auftrag überspringen");
+
+        transactionButton.setActionCommand("list");
+        transactionButton.setSize(250, 50);
+
+
+        nextOrder.setActionCommand("next");
+        nextOrder.setSize(250, 50);
+
+        skipOrder.setActionCommand("skip");
+        skipOrder.setSize(250, 50);
+
+        transactionButton.addActionListener(new ButtonClickListener());
+        nextOrder.addActionListener(new ButtonClickListener());
+        skipOrder.addActionListener(new ButtonClickListener());
+
+        container.add(transactionButton);
+        container.add(nextOrder);
+        container.add(skipOrder);
+        this.r.add(container, BorderLayout.SOUTH);
+    }
+
+    private Order getCurOrder(){
+        return this.o.get(this.currentOrderIndex);
+    }
+
+    private void skipOrder(){
+        Order curOrder = getCurOrder();
+        this.currentOrderIndex++;
+        if(this.currentOrderIndex >= this.o.size()){
+            this.currentOrderIndex = 0;
+        }
+        this.b.updateBalance(curOrder.price, "Auftrag abgelehnt: -" + curOrder.price + "€", true);
+        this.rerenderHud();
+    }
+
+    private void nextOrder(){
+//        if(this.currentOpenOrders <= 3){
+//            this.currentOrderIndex++;
+//            this.currentOpenOrders++;
+//        }
+        if(this.currentOrderIndex >= this.o.size()){
+            this.currentOrderIndex = 0;
+        }
+        this.rerenderHud();
     }
 
     private class ButtonClickListener implements ActionListener{
         public void actionPerformed(ActionEvent e) {
             String command = e.getActionCommand();
-            if(command.equals("View Transactions"))
-                popOutTransactions();
+            switch(command){
+                case "list" -> popOutTransactions();
+                case "next" -> nextOrder();
+                case "skip" -> skipOrder();
+            }
         }
     }
 }
